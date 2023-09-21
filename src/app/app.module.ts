5921e9a89f729142dc6e7334ab07cca9292f3507
G.S <131451577+st-doval17@users.noreply.github.com>
2023-09-21 22:14:21 +0200

    Update Welcome Page Component
    - update app.component.html file
    - update app.component.ts file
    - update app.module.ts file
    - update movie-card.component.html file
    - update user-login-form.component.ts file
    - update welcome-page.component.html file
    - update welcome-page.component.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,14 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
@@ -40,6 +48,8 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
     MatDialogModule,
     MatSnackBarModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { UserRegistrationService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -14,7 +15,8 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
@@ -28,6 +30,7 @@ export class UserLoginFormComponent implements OnInit {
         this.snackBar.open("You've been logged in", 'OK', {
           duration: 2000,
         });
+        this.router.navigate(['movies']);
       },
       error: (data) => {
         this.snackBar.open(data, 'OK', {
