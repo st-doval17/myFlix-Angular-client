edc45f45e99443b9a10e696262baefb1dcc12b67
G.S <131451577+st-doval17@users.noreply.github.com>
2023-09-22 22:00:57 +0200

    Update Movie Details Dialog
    - update movie-details-dialog.component.html
    - update movie-details-dialog.component.ts

--- src/app/movie-details-dialog/movie-details-dialog.component.ts
+++ src/app/movie-details-dialog/movie-details-dialog.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-movie-details-dialog',
   templateUrl: './movie-details-dialog.component.html',
-  styleUrls: ['./movie-details-dialog.component.scss']
+  styleUrls: ['./movie-details-dialog.component.scss'],
 })
 export class MovieDetailsDialogComponent {
-
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 }
